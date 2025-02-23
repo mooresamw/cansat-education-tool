3b664543a01a170dd495b69f1f92dc5490492868
sam <sammoore441@gmail.com>
2025-02-23 14:56:42 -0500

    Wrapping arduino ide in the dashboard layout component for cleaner look

--- app/dashboard/student/ide/page.tsx
+++ app/dashboard/student/ide/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import ArduinoIDE from "@/components/ArduinoIDE"
+import {DashboardLayout} from "@/components/DashboardLayout";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
+        <DashboardLayout userType={"student"}>
       <ArduinoIDE />
+        </DashboardLayout>
     </main>
   )
 }
