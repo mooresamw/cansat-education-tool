4d6ecfdfdeb602601a7e696ce2d3461d4798bf70
sam <sammoore441@gmail.com>
2026-03-19 18:21:54 -0400

    custom hook implementation for health data

--- hooks/useDatabaseHealth.ts
+++ hooks/useDatabaseHealth.ts
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import { doc, onSnapshot } from 'firebase/firestore';
+import {db} from "@/lib/firebaseConfig";
 
+interface HealthData {
+    status: string;
+    avg: number;
+    activeUsers: number;
+    lastCheck: any;
+}
 
-export const useDatabaseHealth = () => {}
+export const useDatabaseHealth = () => {
+    const [data, setData] = useState<HealthData | null>(null);
+    useEffect(() => {
+    const docRef = doc(db,'admin_metrics', 'app_status');
+    const unsubscribe = onSnapshot(docRef, (snapshot) => {
+      if(snapshot.exists()) {
+        setData(snapshot.data() as HealthData);
+      }
+    }, (error) => {
+      console.error("Error listening to health metrics: ", error);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+    return data;
+}
