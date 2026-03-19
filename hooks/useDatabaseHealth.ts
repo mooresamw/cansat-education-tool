b338896253f47f57d1a9344a48809da883166299
sam <sammoore441@gmail.com>
2026-03-19 18:26:25 -0400

    change interface name again

--- hooks/useDatabaseHealth.ts
+++ hooks/useDatabaseHealth.ts
@@ -4,7 +4,7 @@ import {db} from "@/lib/firebaseConfig";
 
 interface HealthData {
     status: string;
-    avgLatency: number;
+    avgLatencyMs: number;
     activeUsers: number;
     lastCheck: any;
 }
