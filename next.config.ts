ffab1de61307e1a590ccb3fb6584cb7f4e8cec2c
sam <sammoore441@gmail.com>
2026-05-13 20:54:20 -0400

    update config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
     images: {
         
       },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         ignoreBuildErrors: true, // Ignores TypeScript type checking errors
     },
