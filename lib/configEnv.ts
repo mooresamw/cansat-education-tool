9c4e1b73b30b6b5a87567746e992cd7149b32c3a
sam <sammoore441@gmail.com>
2026-05-23 17:08:04 -0400

    config

--- lib/configEnv.ts
+++ lib/configEnv.ts
@@ -2,7 +2,10 @@
 Return the API URL base from configured ENV
  */
 function getAPIURLBase(): string {
-    return process.env.NEXT_PUBLIC_API_URL_BASE || "/backend";
+    //return process.env.NEXT_PUBLIC_API_URL_BASE || "/backend";
+    const isDev = process.env.NEXT_PUBLIC_ENV === 'development';
+    return isDev ? 'http://localhost:8080' : '/backend';
+
 }
 
 export const apiUrlBase = getAPIURLBase();
